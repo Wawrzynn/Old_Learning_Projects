308225ac11549577267b504e9488deb83d6e076b
Wawrzynn <76731755+Wawrzynn@users.noreply.github.com>
2021-07-13 12:20:24 +0200

    Delete CS50/pset8 directory

--- CS50/pset8/pset7/movies/movies/11.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN ratings ON 
-ratings.movie_id = stars.movie_id JOIN people ON stars.person_id = people.id
-WHERE name = 'Chadwick Boseman' ORDER BY rating DESC LIMIT 5;
-
-
-
-
